7ec844d8fd51191c2e8f8319a4f35f36bb35bb41
Alexandr Kr <testdert6705@gmail.com>
2019-01-13 18:15:22 +0200

    Registratiin page

--- src/main/java/RegisterPage.java
+++ src/main/java/RegisterPage.java
@@ -1,5 +1,10 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
 
 public class RegisterPage {
 
@@ -24,7 +29,9 @@ public class RegisterPage {
     //Company info
     By companyNameField = By.name("company");
     By industryDropDownList = By.xpath("//*[@id='sector']/div[1]/div[2]");
+    By industrySetDropDownList = By.xpath("/html/body/div[3]/div/div/div/div[1]/span");
     By organizationDropDownList = By.xpath("//*[@id='organization']/div[1]/div[2]");
+    By organizationSetDropDownList = By.xpath("/html/body/div[3]/div/div/div/div[2]/span/div");
     By siteField = By.name("site");
     By addressField = By.name("address");
     By cityField = By.name("city");
@@ -72,4 +79,70 @@ public class RegisterPage {
         driver.findElement(telField).sendKeys(phone);
         return this;
     }
+    public RegisterPage clickNextSecond()
+    {
+        driver.findElement(nextSecondGroupButton).click();
+        return this;
+    }
+    public RegisterPage typeCompanyName(String CompanyName)
+    {
+        driver.findElement(companyNameField).sendKeys(CompanyName);
+        return this;
+    }
+    public RegisterPage setIndustry()
+    {
+        driver.findElement(industryDropDownList).click();
+        new WebDriverWait(driver,10).until(ExpectedConditions.elementToBeClickable(industrySetDropDownList)).click();
+
+        return this;
+    }
+    public RegisterPage setTypeOrganization(String surname)
+    {
+        driver.findElement(organizationDropDownList).click();
+        new WebDriverWait(driver,10).until(ExpectedConditions.elementToBeClickable(organizationSetDropDownList)).click();
+        return this;
+    }
+    public RegisterPage typeSite(String site)
+    {
+        driver.findElement(siteField).sendKeys(site);
+        return this;
+    }
+    public RegisterPage typeAddress(String address)
+    {
+        driver.findElement(addressField).sendKeys(address);
+        return this;
+    }
+    public RegisterPage typeCity(String city)
+    {
+        driver.findElement(cityField).sendKeys(city);
+        return this;
+    }
+    public RegisterPage typeCountry(String country)
+    {
+        driver.findElement(countryField).sendKeys(country);
+        return this;
+    }
+    public RegisterPage typeTelCompany(String telComp)
+    {
+        driver.findElement(telCompanyField).sendKeys(telComp);
+        return this;
+    }
+    public RegisterPage typeEmailCompany(String emailComp)
+    {
+        driver.findElement(emailCompanyField).sendKeys(emailComp);
+        return this;
+    }
+    public RegisterPage setTerms(boolean value)
+    {
+        WebElement checkbox=driver.findElement(termsCheckbox);
+        if(!checkbox.isSelected()==value)
+            checkbox.click();
+        return this;
+    }
+    public void clickRegisterButton()
+    {
+        driver.findElement(registerButton).click();
+    }
+
+
 }
